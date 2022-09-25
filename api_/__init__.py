fce33891edc4c5a637ad8f91fa7e494cc5163915
Tao Xiang <xiang.tao@outlook.de>
2022-09-25 16:22:28 +0200

    ytb-channel

--- api_/__init__.py
+++ api_/__init__.py
@@ -1,6 +1,7 @@
 from api_._weather_query import weather_query as _api1
 from api_._thesis_info import thesis_info as _api2
-
+from ._ytb_channel_info import YTBInfoBot as api3
 
 today_weather = _api1
 crawl_thesis_info = _api2
+YTBInfoBot = api3
