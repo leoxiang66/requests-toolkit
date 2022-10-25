c0adf02ea512e6779655b98daf679e8e341a078d
Tao Xiang <xiang.tao@outlook.de>
2022-10-25 09:37:10 +0200

    1

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["lxml",'requests']
+requirements = [
+    "lxml==4.9.1",
+    'requests==2.28.1',
+    "xmltodict==0.13.0"
+]
 
 setup(
     name="requests-tutorial",
     version="0.4.1",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
-    description="A package of APIs",
+    description="A package of APIs using requests.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/leoxiang66/requests-tutorial",
+    url="https://github.com/leoxiang66/requests-toolkit",
     packages=find_packages(),
     # py_modules=['timedd']
     install_requires=requirements,
