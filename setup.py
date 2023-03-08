7da9b2fd54a4f8894ffad10c405589359a4bad36
Tao Xiang <xiang.tao@outlook.de>
2023-03-08 22:02:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="requests-toolkit-stable",
-    version="0.18.1",
+    version="0.18.2",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package of APIs using requests.",
